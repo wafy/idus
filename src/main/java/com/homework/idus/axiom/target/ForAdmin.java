367c9858bf087043ff3691f72a7dd9942b61bf76
최선혁 <wafy75@gmail.com>
2022-07-26 15:25:53 +0900

    관리자 사용자 구분용 마커인터페이스를 추가하라

---
+++ src/main/java/com/homework/idus/axiom/target/ForAdmin.java
@@ -0,0 +1,7 @@
+package com.homework.idus.axiom.target;
+
+/**
+ * 관리자용 마커인터페이스.
+ */
+public interface ForAdmin {
+}
---
+++ src/main/java/com/homework/idus/axiom/target/ForUser.java
@@ -0,0 +1,7 @@
+package com.homework.idus.axiom.target;
+
+/**
+ * 사용자 전용 마커인터페이스
+ */
+public interface ForUser {
+}
