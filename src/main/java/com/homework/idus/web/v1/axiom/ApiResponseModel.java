d8a5ae786d55b4a6881ca4a639bac417b13ed74e
최선혁 <wafy75@gmail.com>
2022-07-26 11:33:09 +0900

    HTTP 공통 응답 객체를 추가하라

---
+++ src/main/java/com/homework/idus/web/v1/axiom/ApiResponseModel.java
@@ -0,0 +1,20 @@
+package com.homework.idus.web.v1.axiom;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * HTTP 응댑 객체를 정의힙니다.
+ * @param <E>
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponseModel<E> {
+
+    private boolean status;
+    private E data;
+}
