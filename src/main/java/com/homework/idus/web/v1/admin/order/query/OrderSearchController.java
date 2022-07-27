10d3355b0c6ba823a43933239a906c632ce133b2
최선혁 <wafy75@gmail.com>
2022-07-27 22:54:08 +0900

    단일 회원 주문목록의 스웨거 설정을 변경하라

--- src/main/java/com/homework/idus/web/v1/admin/order/query/OrderSearchController.java
+++ src/main/java/com/homework/idus/web/v1/admin/order/query/OrderSearchController.java
@@ -1,12 +1,17 @@
 package com.homework.idus.web.v1.admin.order.query;
 
+import com.homework.idus.core.exception.UserNotFountException;
 import com.homework.idus.core.order.query.Order;
 import com.homework.idus.core.order.query.OrderPageSearcher;
 import com.homework.idus.web.v1.admin.UnauthorizedException;
+import com.homework.idus.web.v1.admin.search.query.UserSearchResponse;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -25,10 +30,14 @@ public class OrderSearchController {
 
     @GetMapping("/{userNo}")
     @ResponseStatus(HttpStatus.OK)
-    @ApiOperation(value = "관리자 주문 목록조회", notes = "단일회원 주문정보를 조회 합니다.")
-    @ApiResponses({
-            @ApiResponse(code = 200, message = "정상응답")
-    })
+    @Operation(summary = "관리자 주문 목록조회", description = "단일회원 주문정보를 조회 합니다.")
+    @io.swagger.v3.oas.annotations.responses.ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "회원 조회 성공",
+                    content = @Content(schema = @Schema(implementation = UserSearchResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "401", description = "토큰 정보 없이 조회",
+                    content = @Content(schema = @Schema(implementation = UnauthorizedException.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "존재하지 않는 회원번호로 조회",
+                    content = @Content(schema = @Schema(implementation = UserNotFountException.class)))})
     public Page<OrderResponse> findAll(@PathVariable Long userNo,
                                        @RequestBody OrderRequest request,
                                        @AuthenticationPrincipal UserDetails userDetails) {
