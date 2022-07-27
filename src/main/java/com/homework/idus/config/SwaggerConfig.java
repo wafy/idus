a542cb2b926039f8b4a42b293f638244b62e4e35
최선혁 <wafy75@gmail.com>
2022-07-27 22:51:33 +0900

    스웨거 설정에서 AuthenticationPrincipal.class를 제외하라

--- src/main/java/com/homework/idus/config/SwaggerConfig.java
+++ src/main/java/com/homework/idus/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.homework.idus.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -35,6 +36,7 @@ public class SwaggerConfig {
     public Docket swaggerApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(this.swaggerInfo())
+                .ignoredParameterTypes(AuthenticationPrincipal.class)
                 .securityContexts(Arrays.asList(securityContext()))
                 .securitySchemes(Arrays.asList(apiKey()))
                 .select()
