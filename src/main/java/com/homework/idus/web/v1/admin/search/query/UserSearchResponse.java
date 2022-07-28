abf36b8e8fd912f729687d3ff59dae5fd9643774
최선혁 <wafy75@gmail.com>
2022-07-28 13:17:37 +0900

    빈값에 대한 유효성을 체크하라

--- src/main/java/com/homework/idus/web/v1/admin/search/query/UserSearchResponse.java
+++ src/main/java/com/homework/idus/web/v1/admin/search/query/UserSearchResponse.java
@@ -3,6 +3,7 @@ package com.homework.idus.web.v1.admin.search.query;
 import com.homework.idus.core.user.Gender;
 import com.homework.idus.core.user.command.User;
 import lombok.Getter;
+import org.apache.commons.lang3.ObjectUtils;
 
 /**
  * 사용자 정보 응답을 담당합니다.
@@ -22,6 +23,8 @@ public class UserSearchResponse {
         this.email = user.getEmail();
         this.mobilePhoneNo = user.getMobilePhoneNo();
         this.gender = user.getGender();
-        this.order = new OrderSearchResponse(user.getOrders().get(0));
+        if (user.getOrders().size() > 0) {
+            this.order = new OrderSearchResponse(user.getOrders().get(0));
+        }
     }
 }
